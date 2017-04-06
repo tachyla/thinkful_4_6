a0f6303abc95059a8e72eee179fdaec518a67b07
Zach Williams <saumanahaii@gmail.com>
2017-04-06 14:44:27 -0400

    Finish the lightbulb drill and add text analyzer files

--- Drill 3/app.js
+++ Drill 3/app.js
@@ -1,3 +1,14 @@
+//Declare the bulbFunction outside of the on method
+function bulbFunction(){
+  $(".lightbulb").removeClass("bulb-on");
+  $(this).addClass("bulb-on");
+}
+$(".lightbulb").on("click",bulbFunction);
+
+/*
+//Declare an anonymous function inside the on method
 $(".lightbulb").on("click",function(event){
-  $(this).toggleClass("bulb-on");
+  $(".lightbulb").removeClass("bulb-on");
+  $(this).addClass("bulb-on");
 });
+*/
